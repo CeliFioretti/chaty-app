7e83036de3cf72a5c81c318ebe60e1af68373774
CeliFioretti <celi.fioretti.12@gmail.com>
2025-09-04 11:35:20 -0300

    agrego sidebar basico

--- src/app/canales/layout.jsx
+++ src/app/canales/layout.jsx
@@ -1,12 +1,27 @@
+'use client'
+
 import SideBar from "@/components/SideBar";
+import { useState } from "react";
 
 export default function CanalesLayout({ children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleSideBar() {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <div>
-        <SideBar/>
-        <main className="ml-44 w-full bg-zinc-800 min-h-screen">
-            {children}
-        </main>
+
+      <SideBar isOpen={isOpen} setIsOpen={setIsOpen} />
+      
+      <button onClick={toggleSideBar} className='fixed top-4 left-4 z-50 bg-amber-400 text-black px-3 py-2 rouded-md'>
+        {isOpen ? 'Cerrar' : 'Abrir'}
+      </button>
+
+      <main className={`bg-zinc-800 min-h-screen flex-1 transition-all duration-300 ${isOpen ? 'pl-52' : 'pl-0'}`}>
+        {children}
+      </main>
     </div>
   );
 }
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,41 +5,42 @@ import { useRouter } from 'next/navigation';
 import React from 'react'
 import Link from 'next/link'
 
-function SideBar() {
+function SideBar({ isOpen, setIsOpen }) {
     const router = useRouter();
     const [autenticando, setAutenticando] = useState(true);
 
     useEffect(() => {
-            const token = sessionStorage.getItem('token');
-            if (!token) {
-                router.push('/login')
-            } else {
-                setAutenticando(false);
-            }
-        }, [])
-    
-        if (autenticando) {
-            return (
-                <div className='flex justify-center items-center h-screen text-white'>
-                    <p>Cargando...</p>
-                </div>
-            );
+        const token = sessionStorage.getItem('token');
+        if (!token) {
+            router.push('/login')
+        } else {
+            setAutenticando(false);
         }
+    }, [])
 
-  return (
-    <aside className='bg-zinc-900 text-white w-64 h-screen p-6 flex flex-col gap-4 fixed top-0 left-0'>
-        <h2 className='text-2xl font-bold mb-6'>Chaty</h2>
-        <nav className='flex flex-col gap-3'>
-            <Link href='/canales' className='hover:text-amber-400'>Canales</Link>
-            <Link href='/perfil' className='hover:text-amber-400'>Perfil</Link>
-            <Link href='/ajustes' className='hover:text-amber-400'>Ajustes</Link>
-            <button onClick={() => {
-                sessionStorage.removeItem('token');
-                window.location.href = '/login'
-            }} className='mt-6 text-red-400 hover:text-red-600'>Cerrar sesión</button>
-        </nav>
-    </aside>
-  )
+    if (autenticando) {
+        return (
+            <div className='flex justify-center items-center h-screen text-white'>
+                <p>Cargando...</p>
+            </div>
+        );
+    }
+
+
+    return (
+        <aside className={`bg-zinc-900 text-white w-52 h-screen pt-20 ps-5 flex flex-col gap-4 fixed top-0 left-0 transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+            <h2 className='text-2xl font-bold mb-2'>Menú</h2>
+            <nav className='flex flex-col gap-3'>
+                <Link href='/canales' className='hover:text-amber-400'>Canales</Link>
+                <Link href='/perfil' className='hover:text-amber-400'>Perfil</Link>
+                <Link href='/ajustes' className='hover:text-amber-400'>Ajustes</Link>
+                <button onClick={() => {
+                    sessionStorage.removeItem('token');
+                    window.location.href = '/login'
+                }} className='mt-4 text-red-400 hover:text-red-600 text-start'>Cerrar sesión</button>
+            </nav>
+        </aside>
+    )
 }
 
 export default SideBar
